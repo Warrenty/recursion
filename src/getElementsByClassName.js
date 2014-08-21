546957b2ef423665f04ec117419de95669ff6fe5
Warrenty <warranty6@gmail.com>
2014-08-21 14:41:03 -0700

    Completed stringifyJSON but stuck on getElementsByClassName
    The code below does not work, either.
    
    var getElementsByClassName = function(className){
    return document.getElementsByClassName(className);
    };

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,21 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+	var elementsArr = [];
+	goThroughNodes(document.body.childNodes);
+	return elementsArr;
+
+	function goThroughNodes(parent) {
+		
+		for (var i = 0; i<parent.length; i++) {
+			var node = parent[i]
+			var nodeClass = node.classList;
+			if (_.contains(nodeClass, className)) {
+				elementsArr.push(node);
+			}
+			if (!(nodeClass === undefined)) {
+				goThroughNodes(node.childNodes);
+			}
+		}
+	}
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var stringified = '';
+	var type = Object.prototype.toString.call(obj).slice(8);
+	function cutComma() {
+		if (stringified.length>1) {
+			stringified = stringified.substr(0,stringified.length-1);
+		};
+	};
+	if (type==='Object]') {
+		stringified += '{';
+		for (var key in obj) {
+			var value = stringifyJSON(obj[key]);
+			if (value) {
+				stringified += '"' + key + '":' + value + ',';
+			};
+		};
+		cutComma();
+		stringified += '}';
+	}else if (type==='Array]') {
+		stringified += '[';
+		for (var i =0; i<obj.length; i++) {
+			stringified += stringifyJSON(obj[i]) + ',';
+		};
+		cutComma();
+		stringified += ']';
+	}else if (type==='String]') {
+		stringified += '"' + obj + '"';
+	}else if (type==='Number]'||type==='Boolean]'||type==='Null]'){
+		stringified += obj;
+	};
+	return stringified;
 };
